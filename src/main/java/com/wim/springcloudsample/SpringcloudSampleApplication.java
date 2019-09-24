20b381f5d7653b0fb049df38f12d06b3bc4ff1b7
wim <m969130721@163.com>
2019-09-24 13:59:35 +0800

    init

---
+++ src/main/java/com/wim/springcloudsample/SpringcloudSampleApplication.java
@@ -0,0 +1,13 @@
+package com.wim.springcloudsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudSampleApplication.class, args);
+    }
+
+}
